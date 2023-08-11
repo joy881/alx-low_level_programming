aadd76acdce9694948b63632f8caf4ed512be3cc
joy881 <joy881>
2023-08-11 15:46:33 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,7 @@
  * Return: 0 when successful
  */
 
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art, /n");
-	return(0);
+int main() {
+    printf("with a proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
