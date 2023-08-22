298edf723206c013f3f7138d5b436490926a3259
joy881 <joy881>
2023-08-22 14:03:07 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,13 @@
  */
 int _strlen(char *s)
 {
-    int longi = 0;
-     while (*s != '\0')
-    {
-        longi++;
-        s++;
-    }
+	int longi = 0;
 
-    return (longi);
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
